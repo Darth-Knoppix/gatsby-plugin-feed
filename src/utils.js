f30466a30a03238955c2a19574777439f56ffd67
Seth Corker <seth.corker@zego.com>
2020-06-07 11:35:11 +0100

    fix(utils): fix broken date parsing
    Published date would default to today

--- src/utils.js
+++ src/utils.js
@@ -3,11 +3,19 @@ export function cleanDate(rawDate) {
     return rawDate;
   }
   if (typeof rawDate === "string") {
-    const parsedDate = Date.parse(rawDate);
+    const dateParts = rawDate.split(/\D+/);
 
-    if (isNaN(parsedDate)) return undefined;
-
-    return new Date();
+    return new Date(
+      Date.UTC(
+        dateParts[0],
+        --dateParts[1],
+        dateParts[2],
+        dateParts[3],
+        dateParts[4],
+        dateParts[5],
+        dateParts[6]
+      )
+    );
   }
 
   return undefined;
